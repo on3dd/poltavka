0b83cce9ad1dc334bfe741ca1721d7e6b3cca645
on3dd <onedeadwave@gmail.com>
2020-11-16 19:23:38 +1000

    client upd

--- components/admin/users/index.tsx
+++ components/admin/users/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Row, Col, Space, Button, Table } from 'antd';
+import React, { useCallback } from 'react';
+import { useRouter } from 'next/router';
+import { Typography, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 const Users: React.FC = () => {
-  return <div className="nigger">i hate users</div>;
+  const router = useRouter();
+
+  const onClick = useCallback(() => {
+    router.push('/admin/users/ordinary');
+  }, [router]);
+
+  return (
+    <div className="users">
+      <Typography.Title>Пользователи</Typography.Title>
+      <Typography.Paragraph style={{ fontSize: '1rem' }}>
+        Для продолжения выберите интересующую вас категорию
+        пользователей в меню слева, либо нажмите на кнопку
+        ниже, чтобы автоматически перейти во категорию
+        "Обычные".
+      </Typography.Paragraph>
+      <Button
+        type="primary"
+        size="large"
+        icon={<UserOutlined />}
+        onClick={onClick}
+      >
+        Перейти к обычным пользователям
+      </Button>
+    </div>
+  );
 };
 
 export default Users;
