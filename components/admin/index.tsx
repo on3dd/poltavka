101d8f61ee3a3d6cbc832f58d02681d169a071bc
on3dd <onedeadwave@gmail.com>
2020-11-16 18:25:58 +1000

    client upd

--- components/admin/index.tsx
+++ components/admin/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import {  } from 'antd';
-
-import RootState from '../../types/states';
+import React, { useCallback } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Typography, Button } from 'antd';
+import { TeamOutlined } from '@ant-design/icons';
 
 const Admin: React.FC = () => {
-  const state = useSelector((state: RootState) => state);
+  const router = useRouter();
 
-  return <code>{JSON.stringify(state, null, 2)}</code>;
+  const onClick = useCallback(() => {
+    router.push('/admin/queue');
+  }, [router]);
+
+  return (
+    <div className="home">
+      <Typography.Title>
+        Добро пожаловать в панель администратора!
+      </Typography.Title>
+      <Typography.Paragraph style={{ fontSize: '1rem' }}>
+        Для начала работы выберите интересующую вас вкладку
+        в меню слева, либо нажмите на кнопку ниже, чтобы
+        автоматически перейти во вкладку "Очередь".
+      </Typography.Paragraph>
+      <Button
+        type="primary"
+        size="large"
+        icon={<TeamOutlined />}
+        onClick={onClick}
+      >
+        Перейти в очередь
+      </Button>
+    </div>
+  );
 };
 
 export default Admin;
